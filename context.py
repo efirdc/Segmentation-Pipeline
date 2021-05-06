f260b93c9ff6681e9022ddd627a9af2f5dc83b70
Cory Efird <cefird101@gmail.com>
2021-05-05 21:39:15 -0600

    Initial commit.

---
+++ context.py
@@ -0,0 +1,162 @@
+import torch
+from datetime import datetime
+
+context_globals = {}
+
+
+# ct is short for "context target"
+# This is used when one component in a context needs a reference to another context component as a parameter
+# Should deprecate this and specify targets with strings and a special prefix instead
+class ct:
+    def __init__(self, tname):
+        self.tname = tname
+
+
+# Holds all components for a pytorch experiment
+# Handles initialization and serialization in a convenient way
+class Context:
+    def __init__(self, device, name=None, parts=None, file_name=None, include=None, patch=None, variables=None,
+                 globals=None):
+
+        if file_name is None:
+            assert name is not None, "name is required for a new context"
+
+        self.globals = globals
+        self.name = name
+        self.device = device
+        self.epoch = 0
+        self.iteration = 0
+        self.creation_time = datetime.now().strftime("%y%m%d-%H%M%S")
+        self.info = {}
+        self.variables = variables if variables is not None else {}
+        self.parts = []
+
+        if parts is not None:
+            self.parts = list(parts)
+            for part in self.parts:
+                part["constructor"] = part["constructor"].__name__
+        elif file_name is not None:
+            checkpoint = torch.load(file_name)
+            if "variables" in checkpoint:
+                old_vars = checkpoint["variables"]
+                if set(self.variables.keys()) != set(old_vars.keys()):
+                    raise ValueError(f"Loading this context requires variables {set(old_vars.keys())}.")
+            self.name = checkpoint["name"]
+            if include is None:
+                self.parts = list(checkpoint["parts"])
+            else:
+                self.parts = [part for part in checkpoint["parts"] if part['name'] in include]
+
+            if patch is not None:
+                for part in self.parts:
+                    if part["name"] in patch:
+                        part["constructor"] = patch[part["name"]]["constructor"].__name__
+                        part["params"] = patch[part["name"]]["params"]
+
+            self.epoch = checkpoint["epoch"]
+            self.iteration = checkpoint["iteration"]
+            self.creation_time = checkpoint["creation_time"]
+            self.info = {}
+            if "info" in checkpoint:
+                self.info = checkpoint["info"]
+
+        self.time_name = f'{self.creation_time}_{self.name}'
+        self.state = {}
+        for part in self.parts:
+            self._init_part(part)
+
+    def __getattr__(self, attr):
+        if attr in self.state:
+            return self.state[attr]
+        else:
+            return None
+
+    def add_part(self, name, constructor, **params):
+        part = dict(name=name, constructor=constructor.__name__, params=params)
+        self.parts.append(part)
+        self._init_part(part)
+
+    def insert_part(self, i, name, constructor, **params):
+        part = dict(name=name, constructor=constructor.__name__, params=params)
+        self.parts.insert(i, part)
+        self._init_part(part)
+
+    def _fix_params(self, params):
+        if isinstance(params, dict):
+            return {k: self._fix_params(v) for k, v in params.items()}
+        if isinstance(params, list):
+            return [self._fix_params(param) for param in params]
+        if isinstance(params, tuple):
+            return tuple(self._fix_params(param) for param in params)
+
+        param = params
+        if isinstance(param, ct):
+            return eval("self." + param.tname)
+        if isinstance(param, str):
+            for variable, value in self.variables.items():
+                if not isinstance(value, str):
+                    continue
+                param = param.replace("$" + variable, value)
+            return param
+        return param
+
+    def _init_part(self, part):
+        name = part["name"]
+        if self.globals is None:
+            constructor = context_globals[part["constructor"]]
+        else:
+            constructor = self.globals[part["constructor"]]
+        params = self._fix_params(part["params"])
+
+        if "optimize_target" in part:
+            target = self.state[part["optimize_target"]]
+            new_obj = constructor(target.parameters(), **params)
+        elif "target" in part:
+            target = self.state[part["target"]]
+            new_obj = constructor(target, **params)
+        else:
+            new_obj = constructor(**params)
+
+        if "state_dict" in part:
+            new_obj.load_state_dict(part["state_dict"])
+
+        if isinstance(new_obj, torch.nn.Module):
+            new_obj = new_obj.to(self.device)
+
+        self.state[name] = new_obj
+
+    def get_part(self, name):
+        return self.state[name]
+
+    def get_parts(self, names):
+        out = []
+        for name in names:
+            if name in self.state:
+                out.append(self.state[name])
+            else:
+                out.append(None)
+        return out
+
+    def save(self, filename):
+        for part in self.parts:
+            if hasattr(self.state[part["name"]], "state_dict"):
+                part["state_dict"] = self.state[part["name"]].state_dict()
+
+        if self.info is None:
+            self.info = {}
+        checkpoint = dict(name=self.name, parts=self.parts, epoch=self.epoch, iteration=self.iteration,
+                          creation_time=self.creation_time, variables=self.variables, info=self.info)
+
+        torch.save(checkpoint, filename)
+
+    def __repr__(self):
+        out = f'{self.name}\n'
+        out += f'{self.creation_time}\n'
+        out += f"epoch {self.epoch}\n"
+        out += f"iteration {self.iteration}\n"
+        for k, v in self.info.items():
+            out += f"{k} {v}\n"
+        out += "\n"
+        for part in self.parts:
+            out += f"{part['name']} {part['constructor']} {part['params']}\n"
+        return out
---
+++ run_segmentation_trainer.py
@@ -0,0 +1,137 @@
+import time
+import argparse
+import tarfile
+import shutil
+from pathlib import Path
+import wandb
+import torch
+
+from context import Context
+from configs import *
+
+
+# Converts a time string with format days-hours:minutes:seconds to the number of seconds (integer)
+# i.e. time_str_to_seconds("2-3:30:5") returns 185405, the number of seconds in 2 days, 3 hours, 30 minutes, 5 seconds
+def time_str_to_seconds(timestr):
+    dash_split = timestr.split("-")
+    assert len(dash_split) == 2
+    D = int(dash_split[0])
+    HMS = dash_split[1]
+
+    colon_split = [int(x) for x in HMS.split(":")]
+    assert len(colon_split) == 3
+    H, M, S = colon_split
+
+    return ((D * 24 + H) * 60 + M) * 60 + S
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Segmentation Network Trainer")
+    parser.add_argument("dataset_path", type=str,
+                        help="Path to the dataset. If the dataset is a .tar file it will be unzipped first."
+                        )
+    parser.add_argument("checkpoints_path", type=str, help="Path to directory for saving checkpoints.")
+    parser.add_argument("config", type=str, default="", help="Defines which configuration to use.")
+    parser.add_argument("--work_path", type=str, default=None,
+                        help="Copy the dataset to this directory before training begins."
+                        )
+    parser.add_argument("--load_checkpoint", type=str, default=None,
+                        help="Path to a training checkpoint to resume training."
+                        )
+    parser.add_argument("--iterations", type=int, default=10000,
+                        help="Number of training iterations. "
+                             "Training may stop early if the training time expires first."
+                        )
+    parser.add_argument("--training_time", type=str, default="28-00:00:00",
+                        help="Length of time to train for. Format: days-hours:minutes:seconds "
+                             "Training may stop early if the number of iterations run out first."
+                        )
+    parser.add_argument("--wandb_project", type=str, default="auto-segmentation",
+                        help="Project name for Weights and Biases logging service."
+                        )
+    args, unknown_args = parser.parse_known_args()
+
+    # Parse extra unknown keyword arguments that were passed on the command line
+    assert len(unknown_args) % 2 == 0, f"Extra arguments have an invalid pairing: {unknown_args}"
+    extra_args = {}
+    for i in range(0, len(unknown_args), 2):
+        name = unknown_args[i]
+        value = unknown_args[i + 1]
+        assert name.startswith('--'), f"Extra arguments must start with '--', found: {name}"
+        extra_args[name[2:]] = value
+
+    # Compute time when training should terminate.
+    # A buffer is used for saving the model (either 5 minutes or 10% of training time, whatever is smaller)
+    training_time = time_str_to_seconds(args.training_time)
+    save_buffer = min(int(training_time * 0.1), 5 * 60)
+    stop_time = time.time() + training_time - save_buffer
+
+    # Extract the dataset if it is a .tar file and copy it to --work_dir if it was specified
+    dataset_path = Path(args.dataset_path)
+    if dataset_path.is_file():
+        assert dataset_path.suffix == '.tar', f"Dataset file extension must be .tar not {dataset_path.suffix}"
+        if args.work_path is None:
+            extract_dir = dataset_path.parent
+        else:
+            extract_dir = Path(args.work_path)
+        extract_dir_contents = [child.stem for child in list(extract_dir.iterdir())]
+        with tarfile.open(name=dataset_path, mode="r") as tar:
+            first_tar_file = tar.getnames()[0]
+            if first_tar_file in extract_dir_contents:
+                print(f"Dataset already extracted to {extract_dir}")
+            else:
+                print(f"Extracting {dataset_path} to {extract_dir}")
+                tar.extractall(extract_dir)
+        dataset_path = extract_dir
+        contents = list(dataset_path.iterdir())
+        if len(contents) == 1:
+            dataset_path = contents[0]
+    elif args.work_path is not None:
+        work_path = Path(args.work_path + "/" + dataset_path.stem)
+        if work_path.exists():
+            print(f"Dataset already transfered to {work_path}")
+        else:
+            print(f"Copying dataset from {dataset_path} to {work_path}")
+            shutil.copytree(dataset_path, work_path)
+        dataset_path = work_path
+    print(f"Using dataset path {dataset_path}")
+
+    # Get device
+    if torch.cuda.is_available():
+        device = torch.device('cuda:0')
+        print("using cuda")
+    else:
+        device = torch.device('cpu')
+        print("not using cuda")
+
+    # Initialize a new context, or load from a file to resume training
+    variables = dict(DATASET_PATH=str(dataset_path), CHECKPOINTS_PATH=args.checkpoints_path)
+    if args.load_checkpoint is None:
+        context = get_context(args.config, device, variables)
+    else:
+        context = Context(device, file_name=args.load_checkpoint, variables=variables, globals=globals())
+
+    # Setup Weights and Biases logging service
+    print(f"initializing wandb")
+    wandb_params = {}
+    if "wandb_id" in context.info:
+        wandb_params["id"] = context.info["wandb_id"]
+        wandb_params["resume"] = "allow"
+    else:
+        wandb_params["id"] = context.info["wandb_id"] = wandb.util.generate_id()
+    for i in range(100):
+        try:
+            wandb.init(project=args.wandb_project, **wandb_params)
+            break
+        except:
+            print(f"wandb init failed. retrying...")
+            time.sleep(10)
+        raise TimeoutError("max wandb init attempts reached.")
+    if not context.name.endswith(wandb.run.name):
+        print(f"Appending wandb run name to context name.")
+        context.name += '-' + wandb.run.name
+
+    print(str(context))
+
+    print("entering training loop")
+    context.trainer.train(context, args.iterations, stop_time=stop_time, wandb_logging=True)
