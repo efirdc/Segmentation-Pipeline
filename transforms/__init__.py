b73c12ef7978f11782c10eb45a54aea4e4f258b1
Cory Efird <cefird101@gmail.com>
2021-06-29 12:29:21 -0600

    New config for MS data.

--- transforms/__init__.py
+++ transforms/__init__.py
@@ -4,3 +4,5 @@ from .custom_label_transforms import CustomRemapLabels, CustomSequentialLabels, 
 from .copy_image import CopyProperty
 from .rename_image import RenameProperty
 from .split_image import SplitImage
+from .crop_to_mask import CropToMask
+from .set_data_type import SetDataType
