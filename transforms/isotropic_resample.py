e54f4a82b188138588c919829ea2fd50ad895b4c
Cory Efird <cefird101@gmail.com>
2021-07-10 20:35:10 -0600

    Add a min_spacing option to IsotropicResample.

--- transforms/isotropic_resample.py
+++ transforms/isotropic_resample.py
@@ -4,7 +4,13 @@ import torchio as tio
 
 
 class IsotropicResample(tio.SpatialTransform):
-    def __init__(self, spacing_mode, tolerance, **kwargs):
+    def __init__(
+            self,
+            spacing_mode: str,
+            tolerance: float,
+            min_spacing: float = None,
+            **kwargs
+    ):
         super().__init__(**kwargs)
         self.spacing_modes = {
             'mean': mean, 'median': median, 'min': min, 'max': max
@@ -16,11 +22,17 @@ class IsotropicResample(tio.SpatialTransform):
         self.spacing_mode = spacing_mode
         self.tolerance = tolerance
         self.kwargs = kwargs
-        self.args_names = ('spacing_mode', 'tolerance',)
+        self.min_spacing = min_spacing
+        self.args_names = ('spacing_mode', 'tolerance', 'min_spacing')
 
     def apply_transform(self, subject):
         spacing = subject.get_first_image().spacing
         isotropic_spacing = self.spacing_modes[self.spacing_mode](spacing)
+
+        if self.min_spacing:
+            increment = isotropic_spacing * 0.5
+            while isotropic_spacing < self.min_spacing:
+                isotropic_spacing += increment
 
         new_spacing = []
         for s in spacing:
