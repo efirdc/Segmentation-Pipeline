2de2e30916c298f12901a2eb34f9079c05e0321d
Cory Efird <cefird101@gmail.com>
2021-07-14 06:32:46 -0600

    New ensembling models.

--- models/__init__.py
+++ models/__init__.py
@@ -1,3 +1,4 @@
 from .nested_residual_unet import NestedResUNet
 from .components import WSConv3d, BlurConv3d, BlurConvTranspose3d, Block3d
 from .modular_unet import ModularUNet
+from .ensemble import EnsembleOrientations, EnsembleModels
