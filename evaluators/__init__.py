caf1b81e33877c83118dfe04fbbc443603102e2c
Cory Efird <cefird101@gmail.com>
2021-07-23 00:37:25 -0600

    Add an evaluator with metrics for instance segmentation tasks (i.e. lesions).

--- evaluators/__init__.py
+++ evaluators/__init__.py
@@ -3,3 +3,4 @@ from .evaluation_dict import EvaluationDict
 from .segmentation_evaluator import SegmentationEvaluator
 from .label_map_evaluator import LabelMapEvaluator
 from .contour_image_evaluator import ContourImageEvaluator
+from .instance_segmentation_evaluator import InstanceSegmentationEvaluator
