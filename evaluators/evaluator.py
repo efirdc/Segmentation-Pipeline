1314af00ad9b59cbbb7d050c4ef416874d99303f
Cory Efird <cefird101@gmail.com>
2021-07-23 00:35:14 -0600

    Remove code that was moved to EvaluationDict.

--- evaluators/evaluator.py
+++ evaluators/evaluator.py
@@ -1,7 +1,6 @@
 from abc import ABC, abstractmethod
 from typing import Sequence
 
-import torch
 import torchio as tio
 
 
@@ -9,21 +8,3 @@ class Evaluator(ABC):
     @abstractmethod
     def __call__(self, subjects: Sequence[tio.Subject]) -> dict:
         raise NotImplementedError()
-
-    @staticmethod
-    def get_summary_stat_funcs(dim: int = 0):
-        return {
-            'mean': lambda x: torch.mean(Evaluator.fix_tensor(x), dim=dim),
-            'median': lambda x: torch.median(Evaluator.fix_tensor(x), dim=dim).values,
-            'mode': lambda x: torch.mode(Evaluator.fix_tensor(x), dim=dim).values,
-            'std': lambda x: torch.std(Evaluator.fix_tensor(x), dim=dim),
-            'min': lambda x: torch.min(Evaluator.fix_tensor(x), dim=dim).values,
-            'max': lambda x: torch.max(Evaluator.fix_tensor(x), dim=dim).values,
-        }
-
-    @staticmethod
-    def fix_tensor(x):
-        x = x[x.isfinite()]
-        if x.shape[0] == 0:
-            return torch.tensor([0.])
-        return x
