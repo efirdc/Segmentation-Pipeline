f287d3a8f375e261b44a2e04bae8172b2d9dd3a8
Cory Efird <cefird101@gmail.com>
2021-07-23 00:36:16 -0600

    Refactor the `SegmentationEvaluator` to use `EvaluationDict`s.

--- evaluators/__init__.py
+++ evaluators/__init__.py
@@ -1,4 +1,5 @@
 from .evaluator import Evaluator
+from .evaluation_dict import EvaluationDict
 from .segmentation_evaluator import SegmentationEvaluator
 from .label_map_evaluator import LabelMapEvaluator
 from .contour_image_evaluator import ContourImageEvaluator
