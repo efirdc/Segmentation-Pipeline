cad2b4aad705fe4cf3519f4daa9e4aca46e29973
Cory Efird <cefird101@gmail.com>
2021-08-05 15:54:08 -0600

    Evaluator can also accept a subject dataset.

--- evaluators/evaluator.py
+++ evaluators/evaluator.py
@@ -1,10 +1,10 @@
 from abc import ABC, abstractmethod
-from typing import Sequence
+from typing import Sequence, Union
 
 import torchio as tio
 
 
 class Evaluator(ABC):
     @abstractmethod
-    def __call__(self, subjects: Sequence[tio.Subject]) -> dict:
+    def __call__(self, subjects: Union[Sequence[tio.Subject], tio.SubjectsDataset]) -> dict:
         raise NotImplementedError()
